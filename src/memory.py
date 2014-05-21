a78f42c8f9ff5dfc25fa848a9a4b33a5561250b7
Gaëtan de Menten <gdementen@gmail.com>
2014-05-21 18:01:53 +0200

    more work on the embryonic memory manager... this approach is probably more work than I can spend on it

--- src/memory.py
+++ src/memory.py
@@ -1,53 +1,120 @@
-from __future__ import division
+from __future__ import division, print_function
 
 import math
+from types import BuiltinFunctionType, FunctionType
 
-import numpy as np
 
 from utils import prod
 
 
 class Manager(object):
-    def __init__(self):
-        pass
+    def __init__(self, cache):
+        self.cache = cache
 
-    def allocate(self, nbytes, func, *args, **kwargs):
+    def safe_call(self, nbytes, func, *args, **kwargs):
+        #TODO: check & free mem beforehand (use psutil)
         try:
             array = func(*args, **kwargs)
         except MemoryError:
             self.free(nbytes)
             array = func(*args, **kwargs)
+            print("succeeded!")
+        # make sure the given size was correct
         assert array.nbytes == nbytes
         return array
 
-    def empty(self, shape, dtype=float, order='C'):
+    def decorate(self, sizefunc, func):
+        def decorated(*args, **kwargs):
+            return self.safe_call(sizefunc(*args, **kwargs),
+                                  func, *args, **kwargs)
+        decorated.__name__ = func.__name__
+        return decorated
+
+    def free(self, nbytes):
+        print("mem is full, puring %d elements from cache" % len(self.cache))
+        self.cache.clear()
+
+
+class ManagedModule(object):
+    def __init__(self, module, manager, sizefuncs):
+        self.module = module
+        self.manager = manager
+        self.sizefuncs = sizefuncs
+
+    def __getattr__(self, key):
+        func = getattr(self.module, key)
+        assert isinstance(func, (BuiltinFunctionType, FunctionType))
+        return self.manager.decorate(self.sizefuncs[key], func)
+
+if __name__ == '__main__':
+    import numpy as np
+
+    def generic(shape, dtype=float, *_, **__):
         if not isinstance(shape, tuple):
             shape = (shape,)
         dt = np.dtype(dtype)
-        nbytes = prod(shape) * dt.itemsize
-        return self.allocate(nbytes, np.empty, shape, dt, order)
+        return prod(shape) * dt.itemsize
 
-    def zeros(self, shape, dtype=float, order='C'):
-        array = self.empty(shape, dtype, order)
-        array[:] = 0
-        return array
+    def like(a, dtype=None, *_, **__):
+        if dtype is None:
+            return a.nbytes
+        else:
+            return generic(a.shape, dtype)
 
-    def ones(self, shape, dtype=float, order='C'):
-        array = self.empty(shape, dtype, order)
-        array[:] = 1
-        return array
-
-    def arange(self, start, stop=None, step=None, dtype=None):
+    def arange(start, stop=None, step=None, dtype=None):
         if dtype is None:
             dtype = type(start)
-        dt = np.dtype(dtype)
         if stop is None:
             start, stop = 0, start
         if step is None:
             step = 1
-        nbytes = int(math.ceil((stop - start) / step)) * dt.itemsize
-        return self.allocate(nbytes, np.arange, start, stop, step)
+        return generic(int(math.ceil((stop - start) / step)), dtype)
 
-    def free(self, nbytes):
-        pass
+    def aggregate_shape(a, axis=None, out=None):
+        if out is not None:
+            return 0
+        elif axis is None:
+            return 1
+        else:
+            if not isinstance(axis, tuple):
+                axis = (axis,)
+            tokill = set(axis)
+            return tuple(size for i, size in enumerate(a.shape)
+                         if i not in tokill)
 
+    def make_aggregate(dt=None):
+        def aggregate_func(a, axis=None, out=None):
+            if dt is None:
+                dt = a.dtype
+            return generic(aggregate_shape(a, axis, out), dt)
+        return aggregate_func
+
+    cache = {}
+    manager = Manager(cache)
+    mnp = ManagedModule(np, manager, {
+        'empty': generic,
+        'zeros': generic,
+        'ones': generic,
+        'arange': arange,
+        'empty_like': like,
+        'copy': like,
+        'sort': like,
+        'all': make_aggregate(bool),
+        'any': make_aggregate(bool),
+        'sum': make_aggregate(),
+        #TODO: array slices et al via ndarray subclass
+        #TODO: array, asarray
+        # agg: cumsum, nansum, min, nanmin, nanmax, std, median, percentile,
+        # argsort, bincount,
+        # insert, delete, concatenate, repeat
+        # intersect1d, union1d, setdiff1d, repeat,
+        # isnan, isfinite, clip, round,
+        # where
+        # random.xxx, unique,
+    }) #, ignored=set('inexact,issubdtype')
+
+    for i in range(50):
+        print(i, end=' ')
+        new = mnp.arange(1e7)
+        print("ok")
+        cache[i] = new
