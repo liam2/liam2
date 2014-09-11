b6f1f7e5ee0a4d9bd338282d272c153aaca2284c
Gaëtan de Menten <gdementen@gmail.com>
2014-09-11 17:44:21 +0200

    use liam2/64 conda env to build the doc

--- doc/buildall.bat
+++ doc/buildall.bat
@@ -3,6 +3,9 @@ rem change directory to the location of this batch file
 rem (but remember original directory)
 pushd %~dp0
 
+call c64.bat
+call activate liam2
+
 rem pushd reference
 rem call buildall.bat
 rem popd
