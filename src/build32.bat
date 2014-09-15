8f6f0e86b5219d434669a9110f3baac32aa962b5
Gaëtan de Menten <gdementen@gmail.com>
2014-09-15 16:06:12 +0200

    document workaround for cx_freeze/pyqt .ui handling clash

--- src/build32.bat
+++ src/build32.bat
@@ -16,4 +16,9 @@ rem    everything is ready to compile C extensions using a 32-bit compiler
 rem    (see setenv32.bat)
 call c32.bat
 call activate liam2
+
+rem cxfreezing an app which loads .ui files fails
+rem https://bitbucket.org/anthony_tuininga/cx_freeze/issue/103
+rem cx-freeze 4.3.4 should fix this, but in the meantime:
+rem rm -rf %ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\Lib\site-packages\PyQt4\uic\port_v3\
 python setup.py build
