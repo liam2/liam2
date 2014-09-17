d6613dc34d0add2c707eb5ca9c92a1bea8f46cc2
Gaëtan de Menten <gdementen@gmail.com>
2014-09-17 16:54:23 +0200

    update setup.py for ViTables 2.1 (and -hopefully- later)

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,10 @@
 from __future__ import print_function
 
+import os
 import sys
+import fnmatch
+from os.path import join
+from itertools import chain
 from distutils.extension import Extension
 
 from cx_Freeze import setup, Executable
@@ -8,54 +12,17 @@ from Cython.Distutils import build_ext
 import numpy as np
 
 
-# cython options
+#===============#
+# generic tools #
+#===============#
 
-# Add the output directory of cython build_ext to sys.path so that build_exe
-# finds and copies C extensions
-class MyBuildExt(build_ext):
-    def finalize_options(self):
-        build_ext.finalize_options(self)
-        sys.path.insert(0, self.build_lib)
-
-
-ext_modules = [Extension("cpartition", ["cpartition.pyx"],
-                         include_dirs=[np.get_include()]),
-               Extension("cutils", ["cutils.pyx"],
-                         include_dirs=[np.get_include()])]
-build_ext_options = {}
-
-
-# cx_freeze options
-build_exe_options = {
-    # compress zip archive
-    "compressed": True,
-
-    # optimize pyc files (strip docstrings and asserts)
-    "optimize": 2,
-
-    # strip paths in __file__ attributes
-    "replace_paths": [("*", "")],
-
-    "includes": ["matplotlib.backends.backend_qt4agg"],
-    # "includes": ["matplotlib.backends.backend_tkagg"],
-
-    # matplotlib => calendar, distutils, unicodedata
-    # matplotlib.backends.backend_tkagg => Tkconstants, Tkinter
-    # ctypes, io are required now
-    "excludes": [
-        # linux-specific modules
-        "_codecs", "_codecs_cn", "_codecs_hk", "_codecs_iso2022", "_codecs_jp",
-        "_codecs_kr", "_codecs_tw",
-
-        # common modules
-        "Tkconstants", "Tkinter", "Cython", "_ssl", "base64", "bz2", "compiler",
-        "doctest", "dummy_thread", "dummy_threading", "email", "ftplib",
-        "logging", "multiprocessing", "nose", "numpy.distutils",
-        "numpy.core._dotblas", "os2emxpath", "pdb", "pkg_resources",
-        "posixpath", "pydoc", "pydoc_topics", "repr", "scipy", "select",
-        "stringprep", "strptime", "tcl", "xml"
-    ]
-}
+def allfiles(pattern, path='.'):
+    """
+    like glob.glob(pattern) but also include files in subdirectories
+    """
+    return (join(dirpath, f)
+            for dirpath, dirnames, files in os.walk(path)
+            for f in fnmatch.filter(files, pattern))
 
 
 def int_version(release_name):
@@ -93,12 +60,84 @@ def int_version(release_name):
     return head + '.' + str(int(middle) - 1) + patch
 
 
-setup(name="liam2", version=int_version('0.8.1'),
-      # cx_freeze wants only ints and dots
+#================#
+# cython options #
+#================#
+
+# Add the output directory of cython build_ext to sys.path so that build_exe
+# finds and copies C extensions
+class MyBuildExt(build_ext):
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        sys.path.insert(0, self.build_lib)
+
+
+ext_modules = [Extension("cpartition", ["cpartition.pyx"],
+                         include_dirs=[np.get_include()]),
+               Extension("cutils", ["cutils.pyx"],
+                         include_dirs=[np.get_include()])]
+build_ext_options = {}
+
+
+#===================#
+# cx_freeze options #
+#===================#
+
+def vitables_data_files():
+    try:
+        import vitables
+    except ImportError:
+        return []
+
+    module_path = os.path.dirname(vitables.__file__)
+    files = chain(allfiles('*.ui', module_path),
+                  allfiles('*.ini', module_path),
+                  allfiles('*', join(module_path, 'icons')))
+    return [(fname, join('vitables', fname[len(module_path) + 1:]))
+            for fname in files]
+
+build_exe_options = {
+    # compress zip archive
+    "compressed": True,
+
+    # optimize pyc files (strip docstrings and asserts)
+    "optimize": 2,
+
+    # strip paths in __file__ attributes
+    "replace_paths": [("*", "")],
+
+    "includes": ["matplotlib.backends.backend_qt4agg"],
+    "packages": ["vitables.plugins"],
+    # matplotlib => calendar, distutils, unicodedata
+    # matplotlib.backends.backend_tkagg => Tkconstants, Tkinter
+    # Qt .ui file loading (for PyTables) => logging, xml
+    # ctypes, io are required now
+    "excludes": [
+        # linux-specific modules
+        "_codecs", "_codecs_cn", "_codecs_hk", "_codecs_iso2022", "_codecs_jp",
+        "_codecs_kr", "_codecs_tw",
+
+        # common modules
+        "Tkconstants", "Tkinter", "Cython", "_ssl", "base64", "bz2", "compiler",
+        "doctest", "dummy_thread", "dummy_threading", "email", "ftplib",
+        "multiprocessing", "nose", "numpy.distutils",
+        "numpy.core._dotblas", "os2emxpath", "pdb", "pkg_resources",
+        "posixpath", "pydoc", "pydoc_topics", "repr", "scipy", "select",
+        "stringprep", "strptime", "tcl"
+    ],
+    'include_files': vitables_data_files(),
+}
+
+
+#============#
+# main stuff #
+#============#
+
+setup(name="liam2",
+      version=int_version('0.8.1'),  # cx_freeze wants only ints and dots
       description="LIAM2",
-
-      cmdclass={"build_ext": MyBuildExt}, ext_modules=ext_modules,
-
+      cmdclass={"build_ext": MyBuildExt},
+      ext_modules=ext_modules,
       options={"build_ext": build_ext_options, "build_exe": build_exe_options},
       executables=[Executable("main.py")],
       requires=['numpy', 'numexpr', 'tables', 'bcolz'])
