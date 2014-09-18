c23a0e21a3fb3095c18018748644bf5c4b006bc1
Gaëtan de Menten <gdementen@gmail.com>
2014-09-18 14:12:43 +0200

    moved "next release changelog" template to its own file and added an example of link to a section.

--- next_release.py
+++ next_release.py
@@ -5,28 +5,12 @@
 from os.path import join
 from make_release import relname2fname
 
-changes_template = """New features
-------------
-
-* added a feature.
-
-* added another feature.
-
-Miscellaneous improvements
---------------------------
-
-* improved something.
-
-Fixes
------
-
-* fixed something (closes :issue:`1`).
-"""
-
 
 def add_release(release_name):
     # create "empty" change file for that release
     fname = relname2fname(release_name)
+    with open(r'doc\usersguide\source\changes\template.rst') as f:
+        changes_template = f.read()
     with open(join(r'doc\usersguide\source\changes', fname), 'w') as f:
         f.write(changes_template)
 
