7727f8aa47aea646212b4990a909ee998f99e56a
Gaëtan de Menten <gdementen@gmail.com>
2014-09-23 12:09:38 +0200

    prepare for release

--- src/setup.py
+++ src/setup.py
@@ -134,7 +134,7 @@ build_exe_options = {
 #============#
 
 setup(name="liam2",
-      version=int_version('0.8.1'),  # cx_freeze wants only ints and dots
+      version=int_version('0.8.2-rc1'),  # cx_freeze wants only ints and dots
       description="LIAM2",
       cmdclass={"build_ext": MyBuildExt},
       ext_modules=ext_modules,
