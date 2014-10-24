3bc05e2eafe6fa050934ce031d81bec065c58c7b
Gaëtan de Menten <gdementen@gmail.com>
2014-10-24 12:30:24 +0200

    no need for a specific __str__ for logit_score

--- src/regressions.py
+++ src/regressions.py
@@ -36,9 +36,6 @@ class LogitScore(CompoundExpression):
             expr = Logistic(BinaryOp('-', expr, epsilon))
         return expr
 
-    def __str__(self):
-        return '%s(%s)' % (self.funcname, self.args[0])
-
     dtype = always(float)
 
 
