c287d184bfdf5e4906619cc7a579d0cbca9f2fe8
Gaëtan de Menten <gdementen@gmail.com>
2015-01-26 08:45:56 +0100

    update copyright year

--- doc/usersguide/source/conf.py
+++ doc/usersguide/source/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'LIAM2 User Guide'
-copyright = u'2011-2014, gb@plan.be, gd@plan.be, gdm@plan.be'
+copyright = u'2011-2015, gb@plan.be, gd@plan.be, gdm@plan.be'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
