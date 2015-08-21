b1eaa173a8ce1b9bb73f9a467b62c83540f71176
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2015-08-21 22:22:12 +0200

    more general tests
    add import and enable a run from python

--- liam2/tests/test_liam2.py
+++ liam2/tests/test_liam2.py
@@ -8,7 +8,7 @@ import pkg_resources
 
 import liam2
 from liam2.simulation import Simulation
-
+from liam2.importer import file2h5
 
 def run_simulation(yaml_file, input_dir, output_dir):
     # try:
@@ -37,6 +37,8 @@ def test_liam2_examples_files():
         'demo_import.yml',  # import file not need to test
         'demo02.yml',  # TODO: pb with figures
         ]
+    excluded_files = [
+        ]
     yaml_files = [os.path.join(liam2_examples_directory, _file) for _file in os.listdir(liam2_examples_directory)
         if os.path.isfile(os.path.join(liam2_examples_directory, _file))
         and _file.endswith('.yml')
@@ -50,7 +52,10 @@ def test_liam2_examples_files():
         'output',
         )
     for yaml_file in yaml_files:
-        yield run_simulation, yaml_file, input_dir, output_dir
+        if 'import' in yaml_file:
+            yield file2h5, yaml_file, input_dir, (10 * 2 ** 20)
+        else:
+            yield run_simulation, yaml_file, input_dir, output_dir
 
 
 def test_liam2_functionnal_files():
@@ -66,8 +71,17 @@ def test_liam2_functionnal_files():
         'tests',
         'output'
         )
-    yaml_files = ['simulation.yml']  # TODO 'import.yml' doesn't work
+    yaml_files = ['simulation.yml']  # TODO 'import.yml' doesn't work
     for yaml_file in yaml_files:
         yaml_path = os.path.join(liam2_functional_directory, yaml_file)
         assert os.path.exists(yaml_path), '{} does not exists'.format(yaml_path)
         yield run_simulation, yaml_path, input_dir, output_dir
+
+if __name__ == '__main__':
+    for function, yaml_file, input_dir, output_dir in test_liam2_examples_files():
+        print (yaml_file)
+        function(yaml_file, input_dir, output_dir)
+
+    for run_simulation, yaml_file, input_dir, output_dir in test_liam2_functionnal_files():
+        print (yaml_file)
+        run_simulation(yaml_file, input_dir, output_dir)
