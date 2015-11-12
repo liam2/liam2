b2799f13ff8933cd261fa63f0f71262b80b0cda4
Gaëtan de Menten <gdementen@gmail.com>
2015-11-12 16:25:11 +0100

    prepare for 0.10.4

--- doc/usersguide/source/changes.rst
+++ doc/usersguide/source/changes.rst
@@ -3,6 +3,14 @@
 Change log
 ##########
 
+Version 0.10.4
+==============
+
+In development.
+
+.. include:: changes/version_0_10_4.rst.inc
+
+
 Version 0.10.3
 ==============
 
--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ if build_exe:
 setup(
     name="liam2",
     # cx_freeze wants only ints and dots
-    version=int_version('0.11.0a1'),
+    version=int_version('0.10.4'),
     description="LIAM2",
     cmdclass={"build_ext": MyBuildExt},
     ext_modules=ext_modules,
