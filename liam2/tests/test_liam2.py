ff6266b6a9cbaf088f763f5384e6eb9811a044e2
Gaëtan de Menten <gdementen@gmail.com>
2018-01-25 12:33:08 +0100

    modernize test runner
    * determine test location from the executed script, instead of relying on the installed liam2 (so that you can test the development version even if you have a "stable" version installed)
    * simulate sets log_level='processes' for all simulations
    * trap stdout & stderr and only show them in cases of failure
    * make sure traceback and "normal" output are not mixed up because of PyCharm
    * show path relative to test_root instead of full path
    * sys.exit(1) if any test fails

--- liam2/tests/test_liam2.py
+++ liam2/tests/test_liam2.py
@@ -2,7 +2,11 @@
 from __future__ import print_function
 
 import os
-import pkg_resources
+import sys
+import traceback
+from StringIO import StringIO
+
+
 from itertools import chain
 
 from liam2.simulation import Simulation
@@ -10,22 +14,60 @@ from liam2.importer import csv2h5
 
 use_travis = os.environ.get('USE_TRAVIS', None) == 'true'
 
-test_root = os.path.join(
-    pkg_resources.get_distribution('liam2').location,
-    'liam2',
-    'tests'
-)
+test_root = os.path.dirname(__file__)
+
+
+def print_title(s):
+    print(s)
+    print("=" * len(s))
+
+
+def simulate(test_file):
+    output_dir = os.path.join(test_root, 'output')
+    simulation = Simulation.from_yaml(test_file, output_dir=output_dir, log_level='processes')
+    simulation.run()
+
+
+def printnow(*args, **kwargs):
+    print(*args, **kwargs)
+    sys.stdout.flush()
 
 
 def run_file(test_file):
-    if 'import' in test_file:
-        print("Importing", test_file)
-        csv2h5(test_file)
+    importing = 'import' in test_file
+    verb = 'Importing' if importing else 'Running'
+    func = csv2h5 if importing else simulate
+
+    printnow('{} {}...'.format(verb, os.path.relpath(test_file, test_root)), end=' ')
+    sys.stdout = StringIO()
+    sys.stderr = StringIO()
+    exception = None
+    try:
+        func(test_file)
+    except Exception, e:
+        exception = e
+    sys.stdout.seek(0)
+    sys.stderr.seek(0)
+    stdout_content = sys.stdout.read()
+    stderr_content = sys.stderr.read()
+    # restore original
+    sys.stdout = sys.__stdout__
+    sys.stderr = sys.__stderr__
+    if exception is None:
+        printnow("done.")
+        return "ok"
     else:
-        output_dir = os.path.join(test_root, 'output')
-        print('Running {} using {} as output dir'.format(test_file, output_dir))
-        simulation = Simulation.from_yaml(test_file, output_dir=output_dir)
-        simulation.run()
+        printnow("FAILED.\n")
+        if stdout_content:
+            printnow("STDOUT\n======\n{}\n".format(stdout_content))
+        if stderr_content:
+            printnow("STDERR\n======\n{}\n".format(stderr_content))
+        # print to stdout to avoid PyCharm randomly mixing up stdout and stderr output
+        printnow("TRACEBACK\n=========")
+        traceback.print_exc(file=sys.stdout)
+        sys.stdout.flush()
+        printnow()
+        return "failed"
 
 
 def iterate_directory(directory, dataset_creator, excluded_files):
@@ -37,23 +79,24 @@ def iterate_directory(directory, dataset_creator, excluded_files):
             yield os.path.join(directory_path, test_file)
 
 
-def test_functional():
-    excluded = ('imported1.yml', 'imported2.yml')
-    if use_travis:
-        excluded += ('static.yml', 'generate.yml')
-    for test_file in iterate_directory('functional', 'import.yml', excluded):
-        yield run_file, test_file
-
-
 def test_examples():
     # No pyqt4 on travis
     need_qt = ('demo02.yml', 'demo03.yml', 'demo04.yml', 'demo06.yml')
     excluded = need_qt if use_travis else ()
     for test_file in iterate_directory('examples', 'demo_import.yml', excluded):
-        yield run_file, test_file
+        yield test_file
+
+
+def test_functional():
+    excluded = ('imported1.yml', 'imported2.yml')
+    if use_travis:
+        excluded += ('static.yml', 'generate.yml')
+    for test_file in iterate_directory('functional', 'import.yml', excluded):
+        yield test_file
 
 
 if __name__ == '__main__':
-   for test in chain(test_examples(), test_functional()):
-        func, args = test[0], test[1:]
-        func(*args)
+    print_title('Using test root: {}'.format(test_root))
+    results = [run_file(f) for f in chain(test_examples(), test_functional())]
+    if any(r == "failed" for r in results):
+        sys.exit(1)
