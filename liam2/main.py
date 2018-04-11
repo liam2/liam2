6f094d1ec0c1b9f8727335b2cc2525855a19e9ea
Gaëtan de Menten <gdementen@gmail.com>
2018-04-11 14:47:34 +0200

    set API v2 for all Qt objects for compatibility with qtpy

--- liam2/main.py
+++ liam2/main.py
@@ -16,6 +16,11 @@ try:
     import sip
     sip.setapi('QString', 2)
     sip.setapi('QVariant', 2)
+    sip.setapi('QDate', 2)
+    sip.setapi('QDateTime', 2)
+    sip.setapi('QTextStream', 2)
+    sip.setapi('QTime', 2)
+    sip.setapi('QUrl', 2)
     del sip
 except ImportError:
     pass
