760d383743f8f1eb73c0227931377ef8550cdfb3
Gaëtan de Menten <gdementen@gmail.com>
2019-10-24 17:14:55 +0200

    use new implementation of getargspec on Python >= 3.3 to avoid problems with numpy >= 1.17

--- liam2/expr.py
+++ liam2/expr.py
@@ -7,7 +7,7 @@ from collections import Counter
 
 import numpy as np
 
-from liam2.compat import basestring, PY2, zip, with_metaclass
+from liam2.compat import basestring, PY2, zip, with_metaclass, getargspec
 from liam2.cache import Cache
 from liam2.context import EntityContext, EvaluationContext
 from liam2.utils import (LabeledArray, ExplainTypeError, safe_take, IrregularNDArray, NiceArgSpec, englishenum, make_hashable,
@@ -672,7 +672,7 @@ class FillArgSpecMeta(FillFuncNameMeta):
                 # >>> inspect.getargspec(a)
                 # ArgSpec(args=['a', 'b', 'c'], varargs='d', keywords='e',
                 #         defaults=(1,))
-                spec = inspect.getargspec(compute)
+                spec = getargspec(compute)
             except TypeError:
                 raise Exception('%s is not a pure-Python function so its '
                                 'signature needs to be specified '
