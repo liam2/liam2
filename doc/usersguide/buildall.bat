9df6a83220b26fb3599c443f46f8be6a6712c331
Gaëtan de Menten <gdementen@gmail.com>
2026-06-09 13:17:35 +0200

    MAINT: update scripts to build documentation
    (disabled html help and PDF for now... probably not worth the effort)

--- doc/usersguide/buildall.bat
+++ doc/usersguide/buildall.bat
@@ -2,13 +2,13 @@
 call make web
 call make html
 
-call make htmlhelp
-pushd build\htmlhelp
-hhc.exe LIAM2UserGuide.hhp > hhc.log
-popd
+rem call make htmlhelp
+rem pushd build\htmlhelp
+rem hhc.exe LIAM2UserGuide.hhp > hhc.log
+rem popd
 
-call make latex
-pushd build\latex
-rem we can also use --run-viewer to open the pdf
-texify.exe --clean --pdf --tex-option=-synctex=1 LIAM2UserGuide.tex > texify.log
-popd
+rem call make latex
+rem pushd build\latex
+rem rem we can also use --run-viewer to open the pdf
+rem texify.exe --clean --pdf --tex-option=-synctex=1 LIAM2UserGuide.tex > texify.log
+rem popd
